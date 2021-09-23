8f3ff7a5543a73b92b9eaada2d6a7ed5d06417d8
sunjiaqi <sunjiaqi>
2021-09-23 10:12:39 +0800

    .

--- sendMail.js
+++ sendMail.js
@@ -1,14 +1,14 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const sendMail = async (data) => {
   let transporter = nodemailer.createTransport({
-    host: 'smtp.qq.com',
-    port: '465',
+    host: "smtp.163.com",
+    port: "465",
     secureConnection: true,
     auth: {
       user: process.env.user,
-      pass: process.env.pass
-    }
+      pass: process.env.pass,
+    },
   });
 
   data.from = `"${data.from}" ${process.env.user}`;
