d380ea2dc151145aa201aebcffb48a9ea6795be5
sjq4499 <sjq4499@163.com>
2022-08-16 14:02:26 +0800

    更新v2free

--- src/juejin.js
+++ src/juejin.js
@@ -1,5 +1,5 @@
-const fetch = require("node-fetch");
-const sendMail = require("./sendMail");
+const fetch = require('node-fetch');
+const sendMail = require('../sendMail');
 
 let [cookie, user, pass, to] = process.argv.slice(2);
 
@@ -8,16 +8,16 @@ process.env.pass = pass;
 let score = 0;
 console.log(cookie, user, pass, to);
 const headers = {
-  "content-type": "application/json; charset=utf-8",
-  "user-agent":
-    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
-  "accept-encoding": "gzip, deflate, br",
-  "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-  "sec-ch-ua":
+  'content-type': 'application/json; charset=utf-8',
+  'user-agent':
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+  'accept-encoding': 'gzip, deflate, br',
+  'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+  'sec-ch-ua':
     '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
-  "sec-ch-ua-mobile": "?0",
-  referer: "https://juejin.cn/",
-  accept: "*/*",
+  'sec-ch-ua-mobile': '?0',
+  referer: 'https://juejin.cn/',
+  accept: '*/*',
   cookie,
 };
 
@@ -25,26 +25,26 @@ const headers = {
 const drawFn = async () => {
   // 查询今日是否有免费抽奖机会
   const today = await fetch(
-    "https://api.juejin.cn/growth_api/v1/lottery_config/get",
+    'https://api.juejin.cn/growth_api/v1/lottery_config/get',
     {
       headers,
-      method: "GET",
-      credentials: "include",
+      method: 'GET',
+      credentials: 'include',
     }
   ).then((res) => res.json());
 
-  if (today.err_no !== 0) return Promise.reject("已经签到！免费抽奖失败！");
+  if (today.err_no !== 0) return Promise.reject('已经签到！免费抽奖失败！');
   if (today.data.free_count === 0)
-    return Promise.resolve("签到成功！今日已经免费抽奖！");
+    return Promise.resolve('签到成功！今日已经免费抽奖！');
 
   // 免费抽奖
-  const draw = await fetch("https://api.juejin.cn/growth_api/v1/lottery/draw", {
+  const draw = await fetch('https://api.juejin.cn/growth_api/v1/lottery/draw', {
     headers,
-    method: "POST",
-    credentials: "include",
+    method: 'POST',
+    credentials: 'include',
   }).then((res) => res.json());
 
-  if (draw.err_no !== 0) return Promise.reject("已经签到！免费抽奖异常！");
+  if (draw.err_no !== 0) return Promise.reject('已经签到！免费抽奖异常！');
   console.log(JSON.stringify(draw, null, 2));
   if (draw.data.lottery_type === 1) score += 66;
   await lucky();
@@ -56,33 +56,33 @@ const drawFn = async () => {
 (async () => {
   // 查询今日是否已经签到
   const today_status = await fetch(
-    "https://api.juejin.cn/growth_api/v1/get_today_status",
+    'https://api.juejin.cn/growth_api/v1/get_today_status',
     {
       headers,
-      method: "GET",
-      credentials: "include",
+      method: 'GET',
+      credentials: 'include',
     }
   ).then((res) => res.json());
 
-  if (today_status.err_no !== 0) return Promise.reject("签到失败！");
-  if (today_status.data) return Promise.resolve("今日已经签到！");
+  if (today_status.err_no !== 0) return Promise.reject('签到失败！');
+  if (today_status.data) return Promise.resolve('今日已经签到！');
 
   // 签到
-  const check_in = await fetch("https://api.juejin.cn/growth_api/v1/check_in", {
+  const check_in = await fetch('https://api.juejin.cn/growth_api/v1/check_in', {
     headers,
-    method: "POST",
-    credentials: "include",
+    method: 'POST',
+    credentials: 'include',
   }).then((res) => res.json());
 
-  if (check_in.err_no !== 0) return Promise.reject("签到异常！");
+  if (check_in.err_no !== 0) return Promise.reject('签到异常！');
   return Promise.resolve(`签到成功！当前积分；${check_in.data.sum_point}`);
 })()
   .then((msg) => {
     console.log(msg);
-    return fetch("https://api.juejin.cn/growth_api/v1/get_cur_point", {
+    return fetch('https://api.juejin.cn/growth_api/v1/get_cur_point', {
       headers,
-      method: "GET",
-      credentials: "include",
+      method: 'GET',
+      credentials: 'include',
     }).then((res) => res.json());
   })
   .then((res) => {
@@ -91,11 +91,11 @@ const drawFn = async () => {
     return drawFn();
   })
   .then((msg) => {
-    console.log(msg, "msg");
+    console.log(msg, 'msg');
     return sendMail({
-      from: "掘金",
+      from: '掘金',
       to,
-      subject: "定时任务成功",
+      subject: '定时任务成功',
       html: `
         <h1 style="text-align: center">自动签到通知</h1>
         <p style="text-indent: 2em">签到结果：${msg}</p>
@@ -104,14 +104,14 @@ const drawFn = async () => {
     }).catch(console.error);
   })
   .then(() => {
-    console.log("邮件发送成功！");
+    console.log('邮件发送成功！');
   })
   .catch((err) => {
-    console.log(err, "err");
+    console.log(err, 'err');
     sendMail({
-      from: "掘金",
+      from: '掘金',
       to,
-      subject: "定时任务失败",
+      subject: '定时任务失败',
       html: `
         <h1 style="text-align: center">自动签到通知</h1>
         <p style="text-indent: 2em">执行结果：${err}</p>
@@ -126,13 +126,13 @@ const drawFn = async () => {
 //  ?aid=&uuid=
 const lucky = async () => {
   const res = await fetch(
-    "https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky",
+    'https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky',
     {
       headers,
-      method: "POST",
-      credentials: "include",
-      body: JSON.stringify({ lottery_history_id: "7052109119238438925" }),
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({ lottery_history_id: '7052109119238438925' }),
     }
   ).then((res) => res.json());
-  console.log(res, "llll");
+  console.log(res, 'llll');
 };
---
+++ src/v2free.js
@@ -0,0 +1,99 @@
+const fetch = require('node-fetch');
+const sendMail = require('../sendMail');
+const v2freeData = {
+  title: 'v2free', // 标题
+  h1: '自动签到', // h1标题
+  notice: '通知',
+  status: '', // 状态 成功 or 失败
+  msg: '', // eg: "获得了 495MB 流量."
+  traffic: '', // "3.29GB"
+  trafficInfo: {
+    // lastUsedTraffic: "142.77MB"  todayUsedTraffic: "0B" unUsedTraffic: "3.16GB"
+    lastUsedTraffic: '',
+    todayUsedTraffic: '',
+    unUsedTraffic: '',
+  },
+  unflowtraffic: '', // nflowtraffic: 3537895424
+  set ret(value) {
+    if (value === 1) {
+      this.status = '成功';
+    } else {
+      this.status = '失败';
+    }
+  },
+  get ret() {
+    return this.status;
+  },
+};
+
+let [cookie2, user, pass, to] = process.argv.slice(2);
+process.env.user = user;
+process.env.pass = pass;
+
+const headers = {
+  'content-type': 'application/json; charset=utf-8',
+  accept: 'application/json, text/javascript, */*; q=0.01',
+  Cookie: cookie2,
+};
+
+/**
+ * 签到
+ * @returns {Promise<object>}
+ */
+async function checkIn() {
+  try {
+    const check_in = await fetch('https://w1.v2dns.xyz/user/checkin', {
+      headers,
+      method: 'POST',
+    }).then((res) => res.json());
+    /*eg: {"msg":"\u83b7\u5f97\u4e86 495MB \u6d41\u91cf.","unflowtraffic":3537895424,"traffic":"3.29GB","trafficInfo":{"todayUsedTraffic":"0B","lastUsedTraffic":"142.77MB","unUsedTraffic":"3.16GB"},"ret":1}*/
+    console.log(check_in, 'check_in');
+    Object.assign(v2freeData, check_in);
+    return v2freeData;
+  } catch (e) {
+    console.error('e:', e);
+  }
+}
+
+/**
+ * 获取发送邮件之后的状态
+ * @param traffic { string }
+ * @param msg { string }
+ * @param lastUsedTraffic {string}
+ * @param todayUsedTraffic { string}
+ * @param unUsedTraffic {string}
+ * @returns {Promise<void>}
+ */
+async function getMsgStatus({
+  traffic = '',
+  msg,
+  trafficInfo: { lastUsedTraffic, todayUsedTraffic, unUsedTraffic },
+}) {
+  const params = {
+    from: v2freeData.title,
+    to,
+    subject: v2freeData.h1 + v2freeData.status,
+    html: `
+        <h1 style="text-align: center">自动签到通知</h1>
+        <p style="text-indent: 2em">签到结果：${msg}</p>
+		`,
+  };
+  if (traffic) {
+    params.html += `
+			<p  style="text-indent: 2em">全部获得${traffic}</p>
+			<p  style="text-indent: 2em">已使用${lastUsedTraffic}</p>
+			<p  style="text-indent: 2em">今日使用${todayUsedTraffic}</p>
+			<p  style="text-indent: 2em">签到之前剩余${unUsedTraffic}</p>
+		`;
+  }
+  return sendMail(params);
+}
+
+(async function () {
+  const repData = await checkIn();
+  try {
+    await getMsgStatus(repData);
+  } catch (e) {
+    console.error(v2freeData.error);
+  }
+})();
