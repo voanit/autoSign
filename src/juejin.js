a87b8b2d3e6e673bfa6bb4ca2d49ffa0f7eef813
sjq4499 <sjq4499@163.com>
2023-01-11 15:15:14 +0800

    唉=

--- src/juejin.js
+++ src/juejin.js
@@ -137,89 +137,89 @@ const lucky = async () => {
   console.log(res, 'llll');
 };
 
-// 第二个掘金签到
-setTimeout(() => {
-  headers = {
-    'content-type': 'application/json; charset=utf-8',
-    'user-agent':
-      'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-    'sec-ch-ua':
-      '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
-    'sec-ch-ua-mobile': '?0',
-    referer: 'https://juejin.cn/',
-    accept: '*/*',
-    cookie: yun_cookie,
-  };
+// // 第二个掘金签到
+// setTimeout(() => {
+//   headers = {
+//     'content-type': 'application/json; charset=utf-8',
+//     'user-agent':
+//       'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+//     'accept-encoding': 'gzip, deflate, br',
+//     'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+//     'sec-ch-ua':
+//       '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+//     'sec-ch-ua-mobile': '?0',
+//     referer: 'https://juejin.cn/',
+//     accept: '*/*',
+//     cookie: yun_cookie,
+//   };
 
-  (async () => {
-    // 查询今日是否已经签到
-    const today_status = await fetch(
-      'https://api.juejin.cn/growth_api/v1/get_today_status',
-      {
-        headers,
-        method: 'GET',
-        credentials: 'include',
-      }
-    ).then((res) => res.json());
+//   (async () => {
+//     // 查询今日是否已经签到
+//     const today_status = await fetch(
+//       'https://api.juejin.cn/growth_api/v1/get_today_status',
+//       {
+//         headers,
+//         method: 'GET',
+//         credentials: 'include',
+//       }
+//     ).then((res) => res.json());
 
-    if (today_status.err_no !== 0) return Promise.reject('签到失败！');
-    if (today_status.data) return Promise.resolve('今日已经签到！');
+//     if (today_status.err_no !== 0) return Promise.reject('签到失败！');
+//     if (today_status.data) return Promise.resolve('今日已经签到！');
 
-    // 签到
-    const check_in = await fetch(
-      'https://api.juejin.cn/growth_api/v1/check_in',
-      {
-        headers,
-        method: 'POST',
-        credentials: 'include',
-      }
-    ).then((res) => res.json());
+//     // 签到
+//     const check_in = await fetch(
+//       'https://api.juejin.cn/growth_api/v1/check_in',
+//       {
+//         headers,
+//         method: 'POST',
+//         credentials: 'include',
+//       }
+//     ).then((res) => res.json());
 
-    if (check_in.err_no !== 0) return Promise.reject('签到异常！');
-    return Promise.resolve(`签到成功！当前积分；${check_in.data.sum_point}`);
-  })()
-    .then((msg) => {
-      console.log(msg);
-      return fetch('https://api.juejin.cn/growth_api/v1/get_cur_point', {
-        headers,
-        method: 'GET',
-        credentials: 'include',
-      }).then((res) => res.json());
-    })
-    .then((res) => {
-      console.log(res);
-      score = res.data;
-      return drawFn();
-    })
-    .then((msg) => {
-      console.log(msg, 'msg');
-      return sendMail({
-        from: '掘金',
-        to: yun_to,
-        subject: '定时任务成功-云',
-        html: `
-          <h1 style="text-align: center">自动签到通知</h1>
-          <p style="text-indent: 2em">签到结果：${msg}</p>
-          <p style="text-indent: 2em">当前积分：${score}</p><br/>
-        `,
-      }).catch(console.error);
-    })
-    .then(() => {
-      console.log('邮件发送成功！');
-    })
-    .catch((err) => {
-      console.log(err, 'err');
-      sendMail({
-        from: '掘金',
-        to: yun_to,
-        subject: '定时任务失败-云',
-        html: `
-          <h1 style="text-align: center">自动签到通知</h1>
-          <p style="text-indent: 2em">执行结果：${err}</p>
-          <p style="text-indent: 2em">当前积分：${score}</p><br/>
-        `,
-      }).catch(console.error);
-    });
-}, Math.floor(Math.random() * 10 * 60 * 1000) + 30 * 1000);
+//     if (check_in.err_no !== 0) return Promise.reject('签到异常！');
+//     return Promise.resolve(`签到成功！当前积分；${check_in.data.sum_point}`);
+//   })()
+//     .then((msg) => {
+//       console.log(msg);
+//       return fetch('https://api.juejin.cn/growth_api/v1/get_cur_point', {
+//         headers,
+//         method: 'GET',
+//         credentials: 'include',
+//       }).then((res) => res.json());
+//     })
+//     .then((res) => {
+//       console.log(res);
+//       score = res.data;
+//       return drawFn();
+//     })
+//     .then((msg) => {
+//       console.log(msg, 'msg');
+//       return sendMail({
+//         from: '掘金',
+//         to: yun_to,
+//         subject: '定时任务成功-云',
+//         html: `
+//           <h1 style="text-align: center">自动签到通知</h1>
+//           <p style="text-indent: 2em">签到结果：${msg}</p>
+//           <p style="text-indent: 2em">当前积分：${score}</p><br/>
+//         `,
+//       }).catch(console.error);
+//     })
+//     .then(() => {
+//       console.log('邮件发送成功！');
+//     })
+//     .catch((err) => {
+//       console.log(err, 'err');
+//       sendMail({
+//         from: '掘金',
+//         to: yun_to,
+//         subject: '定时任务失败-云',
+//         html: `
+//           <h1 style="text-align: center">自动签到通知</h1>
+//           <p style="text-indent: 2em">执行结果：${err}</p>
+//           <p style="text-indent: 2em">当前积分：${score}</p><br/>
+//         `,
+//       }).catch(console.error);
+//     });
+// }, Math.floor(Math.random() * 10 * 60 * 1000) + 30 * 1000);
